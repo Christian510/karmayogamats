5c08d0107653d090eb2cecb20077fcf206ac4e63
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2019-03-02 12:49:29 -0700

    set up endpoints added suggestions and description to yoga_api

--- server/controller.js
+++ server/controller.js
@@ -1,9 +1,46 @@
+
 var yogaApi = require( './yoga_api.json' );
+
+let yogaPose = {};
 
 module.exports = {
 
-    read: (req, res) => {
+    getAllPoses: (req, res) => {
         res.status(200).send( yogaApi );
+    },
+    
+    getEnglish: (req, res ) => {
+        for( var i = 0; i < yogaApi.length; i++ ) {
+            if( yogaApi[i].english_name === req.params.english_name ){
+                yogaPose = {
+                    id:             yogaApi[i].id,
+                    sanskrit_name:  yogaApi[i].sanskrit_name,
+                    english_name:   yogaApi[i].english_name,
+                    img_url:        yogaApi[i].img_url,
+                    description:    yogaApi[i].description,
+                    suggestions:    yogaApi[i].suggestions,
+                    user_id:        yogaApi[i].user_id,
+                }
+            }
+        }
+            res.status( 200 ).send( yogaPose );
+    },
+
+    getSanskrit: (req, res ) => {
+        for( var i = 0; i < yogaApi.length; i++ ) {
+            if( yogaApi[i].sanskrit_name === req.params.sanskrit_name ){
+                yogaPose = {
+                    id:             yogaApi[i].id,
+                    sanskrit_name:  yogaApi[i].sanskrit_name,
+                    english_name:   yogaApi[i].english_name,
+                    img_url:        yogaApi[i].img_url,
+                    description:    yogaApi[i].description,
+                    suggestions:    yogaApi[i].suggestions,
+                    user_id:        yogaApi[i].user_id,
+                }
+            }
+        }
+            res.status( 200 ).send( yogaPose );
     }
 
 }
--- server/index.js
+++ server/index.js
@@ -3,17 +3,21 @@ const bodyParser = require( 'body-parser' );
 const express = require( 'express' );
 const app = express();
 const controller = require( './controller.js' );
+// const cors = require( 'cors' );
 
 //TODO cors is messing with the response.
-
+// app.use( cors() );
 app.use( bodyParser.json() );
+
 // massive( process.env.DATABASE_URL )
 // .then( db => { app.set( db, 'db' ) } )
 // .catch(err => { console.log( err ) });
 
-app.get('/api/yoga_api/', controller.read );
+app.get('/api/yoga_api/', controller.getAllPoses );
 
-app.get( '/api/yoga_api/:' )
+app.get( '/api/yoga_api/english/:english_name', controller.getEnglish);
+
+app.get( '/api/yoga_api/sanskrit/:sanskrit_name', controller.getSanskrit );
 
 const port = process.env.PORT;
 app.listen( port, () => { console.log(`Listening on port ${port}.`); } );
