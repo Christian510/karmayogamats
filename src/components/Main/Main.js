dfd28dd01d85b0b3400623283a34f65510b2be93
Christian's Mac <macbook@Macbooks-MBP.lan>
2019-10-21 15:18:27 -0600

    added images to main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,72 +12,77 @@ function Main() {
     return (
         <div>
             <div className="home-container">
-                <section className="hero flex-center-col">
-                    <h1>Build Your Yoga Class</h1>
-                    <p>Put your class together today</p>
-                    <div>
-                        <button className="button-outlined">
-                            <Link to={'/about'}  >Learn More</Link>
-                        </button>
-                        <button className={"button-solid"}>
-                            <Link to={'/register'} >Start Building</Link>
-                        </button>
-
+                <section className="hero" id="hero-image">
+                    <div className="flex-center-col content-container">
+                        <h1>Build Your Yoga Class</h1>
+                        <p>Put your class together today</p>
+                        <div>
+                            <button className="button-outlined">
+                                <Link to={'/about'}  >Learn More</Link>
+                            </button>
+                            <button className={"button-solid"}>
+                                <Link to={'/register'} >Start Building</Link>
+                            </button>
+                        </div>
                     </div>
                 </section>
                 <section className="get-started flex-center-col">
                     <h1>Loving it is easy</h1>
                     <div className="flex-center-row">
-                        <div className="headings">
+                        <div className="headings flex-center-col ">
                             <img className="lg-dot" src={YogaClass} alt="Yoga Class" ></img>
                             <h2>Create</h2>
                             <p>Create yoga sequences for a class or personal use.</p>
                         </div>
-                        <div className="headings">
-                            <img className="lg-dot" src={PhotoFive} alt="Yoga with IPad" ></img>
+                        <div className="headings flex-center-col ">
+                            <img className="lg-dot" src={PhotoFive} alt="Yoga with IPad"></img>
                             <h2>Customize</h2>
                             <p>48 poses to choose from.</p>
                         </div>
-                        <div className="headings">
-                            <img className="lg-dot" src={PhotoOne} alt="Yoga Pose" ></img>
+                        <div className="headings flex-center-col ">
+                            <img className="lg-dot" src={PhotoOne} alt="Yoga Pose"></img>
                             <h2>Save and Share</h2>
                             <p>Create and save up to 10 sequences.</p>
                         </div>
                     </div>
-                    <button className={"button-solid"}>
-                        <Link to={'/register'} >Start Building</Link>
-                    </button>
-                </section>
-                <section className="content flex-center-col">
-                    <h1>How It Works</h1>
-                    <div className="flex-center-row">
-                        <span className="sm-dot"></span>
-                        <div className="how-it-works">
-                            <h2>Step 1</h2>
-                            <p>Sign up for a new account.</p>
-                            <p>lorem ipsom lorem ipsom</p>
-                        </div>
-                    </div>
-                    <div className="flex-center-row">
-                        <span className="sm-dot"></span>
-                        <div className="how-it-works">
-                            <h2>Step 2</h2>
-                            <p>Create your first yoga sequence</p>
-                            <p>lorem ipsom lorem ipsom</p>
-                        </div>
-                    </div>
-                    <div className="flex-center-row">
-                        <span className="sm-dot"></span>
-                        <div className="how-it-works">
-                            <h2>Step 3</h2>
-                            <p>Save it and do yoga</p>
-                            <p>lorem ipsom lorem ipsom</p>
-                        </div>
-                    </div>
-                    <div id="content-bt">
-                        <button className={"button-solid"} >
-                            <Link to={'/register'}>Build Your First Class</Link>
+                    <div id="center-btn">
+                        <button className={"button-solid"}>
+                            <Link to={'/register'} >Start Building</Link>
                         </button>
+                    </div>
+                </section>
+                <section className="content content-bg-img">
+                    <div className="flex-center-col content-container" >
+                        <h1>How It Works</h1>
+                        <div className="flex-center-row">
+                            <span className="sm-dot"></span>
+                            <div className="how-it-works">
+                                <h2>Step 1</h2>
+                                <p>Sign up for a new account.</p>
+                                <p>lorem ipsom lorem ipsom</p>
+                            </div>
+                        </div>
+                        <div className="flex-center-row">
+                            <span className="sm-dot"></span>
+                            <div className="how-it-works">
+                                <h2>Step 2</h2>
+                                <p>Create your first yoga sequence</p>
+                                <p>lorem ipsom lorem ipsom</p>
+                            </div>
+                        </div>
+                        <div className="flex-center-row">
+                            <span className="sm-dot"></span>
+                            <div className="how-it-works">
+                                <h2>Step 3</h2>
+                                <p>Save it and do yoga</p>
+                                <p>lorem ipsom lorem ipsom</p>
+                            </div>
+                        </div>
+                        <div id="content-bt">
+                            <button className={"button-solid"} >
+                                <Link to={'/register'}>Build Your First Class</Link>
+                            </button>
+                        </div>
                     </div>
                 </section>
             </div>
